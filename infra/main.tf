b7f846bdfba997476ea34dc34c826d51cbb8edbf
aedifex <aedifex@gmail.com>
2026-08-24 17:23:20 -0700

    Update infra code

---
+++ infra/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-iacm-hello-world"
+}
+
+variable "location" {
+  description = "Azure region for all resources"
+  type        = string
+  default     = "eastus"
+}
+
+variable "storage_account_name" {
+  description = "Globally unique name for the storage account (lowercase, no hyphens, 3-24 chars)"
+  type        = string
+  default     = "saiacmhelloworld001"
+}
+
+variable "container_name" {
+  description = "Name of the blob container (the 'bucket')"
+  type        = string
+  default     = "hello-world-container"
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "this" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_storage_container" "this" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.this.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.this.name
+}
